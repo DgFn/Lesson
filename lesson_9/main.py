0b83f88f54a85d739b699c9e159962537214e8d6
DgFn <nkita223@mail.ru>
2024-06-26 00:13:14 +0300

    ДЗ 10 декоратор на main

--- lesson_9/main.py
+++ lesson_9/main.py
@@ -1,8 +1,27 @@
+"""Домашка до среды:
+
+Написать декоратор для функции main, в котором будет в бесконечном цикле вызов
+main до тех пор, пока main функция не вернет значение True.
+True значение должно вернуться в том случае, если в main не было
+ни одной ошибки авторизации и пользователь был успешно авторизовано/зарегистрирован
+"""
+
 from authenticator import Authenticator
 from exceptions import AuthorizationError, RegistrationError
 
 
-def main() -> str:
+def cycle(func):
+    def wrapper():
+        while True:
+
+            if func():
+                return
+
+    return wrapper
+
+
+@cycle
+def main() -> bool:
     """Запуск всех модулей"""
 
     authentication = Authenticator()
@@ -10,31 +29,30 @@ def main() -> str:
         print('Вам нужно авторизоваться')
     else:
         print('Вам нужно зарегистрироватся')
-    while True:
-        login = input('Введите логин:')
-        password = input('Введите пароль:')
-        if authentication.login:
-            try:
-                authentication.authorize(login, password)
-            except AuthorizationError as e:
-                print(e)
-                continue
-            result = (f'Добро пожаловать {authentication.login.strip()}!\nВремя в которое вы зашли: '
-                      f'{authentication.last_success_login_at.strftime('%H:%M:%S')}\n'
-                      f'Количество неверных попыток:{authentication.errors_count}')
-            print(result)
-            return result
 
+    login = input('Введите логин:')
+    password = input('Введите пароль:')
+
+    if authentication.login:
         try:
-            authentication.registrate(login, password)
-
-        except RegistrationError as e:
+            authentication.authorize(login, password)
+        except AuthorizationError as e:
             print(e)
-            continue
-        print('Вы успешно зарегистрированны, авторизуйтесь!')
+            return False
+        result = (f'Добро пожаловать {authentication.login.strip()}!\nВремя в которое вы зашли: '
+                  f'{authentication.last_success_login_at.strftime('%H:%M:%S')}\n'
+                  f'Количество неверных попыток:{authentication.errors_count}')
+        print(result)
+        return True
 
+    try:
+        authentication.registrate(login, password)
 
-# регистрация отдельно, авторизацая в блоке ry
+    except RegistrationError as e:
+        print(e)
+        return False
+    print('Вы успешно зарегистрированны, авторизуйтесь!')
+
 
 if __name__ == '__main__':
     main()
